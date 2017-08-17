aa1214c9a956c710ef43f575806659c763df9f39
francasense <francasense@gmail.com>
2017-08-17 18:00:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50))
     name_user = db.Column(db.String(50))
-    titulo = db.Column(db.String(30))
+    titulo = db.Column(db.String(50))
     valor_inicial = db.Column(db.Float)
     data_inicial = db.Column(db.String(50))
     data_entrega = db.Column(db.String(50))
